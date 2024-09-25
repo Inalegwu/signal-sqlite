209b2167150511a59150b98c4f4d668ab043f940
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-09-25 05:22:13 +0100

    workspace config

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,7 @@
 import { Collection } from "signaldb";
 import createSqlitePersistenceAdapter from "./index.ts";
 
+// type signature of the collection
 type Contact = {
 	id: string;
 	name: string;
@@ -8,7 +9,12 @@ type Contact = {
 };
 
 const collection = new Collection<Contact>({
+	// create the persistence adapter directed at a
+	// database @ database path
 	persistence: createSqlitePersistenceAdapter("fake.db", (builder) => {
+		// set the table column definitions from
+		// here to ensure it is available in the
+		// builder context within the package
 		builder.setColumnDefinitions({
 			id: "TEXT",
 			name: "TEXT",
